cdae22c7168a9ee9243dc30762be09d7586af3c6
duygu <duyguyilmaz032@gmail.com>
2023-01-10 11:15:43 +0300

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,35 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifytripsplanner": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "amplifytripsplanner112eca49": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger282b7f41": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "s3bf9100ad": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
